681438088eb99edaef555e10beb06b11c4855620
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-03-30 18:43:19 +0900

    Feat : print accuracy score & count time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const video = document.getElementById('video')
-
+let count = 0
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -21,11 +21,18 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    if (detections.length!=0){
+      console.log(detections[0]['detection']['_score']); //정확도 정보 제공
+      console.log(`${count/10}초간 보임`);
+      count++;
+    }
+    else{
+      count=0;
+    }
   }, 100)
 })
